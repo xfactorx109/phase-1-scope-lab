d99c311a442a0802d028423795c6f6fc37dc21c3
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2024-02-25 19:35:16 -0800

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'Alice';
+    leastFavoriteCustomer = 'Bob';
+}
